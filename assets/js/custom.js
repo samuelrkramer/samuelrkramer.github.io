9ca157ea376653c1b1b97c5c426bdaeab25b30bf
Sam Kramer <SamuelRKramer@gmail.com>
2022-08-10 11:49:46 -0400

    this more or less works as a loading modal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,8 +2,8 @@ console.log("test, custom is loaded");
 
 const body = document.body;
 const menu = document.getElementById("menu");
+const projLink = document.getElementById("projLink");
 const menuContents = menu.innerHTML;
-const modalContents = document.getElementById("modal").innerHTML;
 
 const listenProjs = () => {
   console.log("listenProjs fired");
@@ -17,14 +17,16 @@ const listenProjs = () => {
 
 const projClickHandler = url => {
   // const hideModal = () => {
-  //   console.log("body click")
+    //   console.log("body click")
   //   body.classList.remove('is-modal-visible')
   //   console.log("modal class unset");
   // }
-
+  
   return async e => {
     e.preventDefault();
     console.log("projClickHandler fired for url", url);
+    projLink.href = url;
+    const modalContents = document.getElementById("modal").innerHTML;
     menu.innerHTML = modalContents;
     body.classList.add('is-menu-visible');
     // body.addEventListener("click", hideModal, {once: true});
@@ -34,6 +36,9 @@ const projClickHandler = url => {
     console.log("sending fetch to", apiUrl, Date.now());
     const res = await fetch(apiUrl);
     console.log("got something back maybe? idk, we're doing it live", Date.now())
+    if (body.classList.contains("is-menu-visible")) {
+      window.open(url, "_blank");
+    }
     body.classList.remove('is-menu-visible');
     menu.innerHTML = menuContents;
   }
