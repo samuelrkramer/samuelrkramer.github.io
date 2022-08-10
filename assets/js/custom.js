d61c316356e540d51f3119956d681968ac876adb
Sam Kramer <SamuelRKramer@gmail.com>
2022-08-10 11:46:59 -0400

    new idea. we're hijacking the whole damn menu modal. I meant to commit this yesterday but forgot to hit enter lol

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,9 @@
 console.log("test, custom is loaded");
 
 const body = document.body;
+const menu = document.getElementById("menu");
+const menuContents = menu.innerHTML;
+const modalContents = document.getElementById("modal").innerHTML;
 
 const listenProjs = () => {
   console.log("listenProjs fired");
@@ -11,18 +14,28 @@ const listenProjs = () => {
   }
 }
 
+
 const projClickHandler = url => {
+  // const hideModal = () => {
+  //   console.log("body click")
+  //   body.classList.remove('is-modal-visible')
+  //   console.log("modal class unset");
+  // }
+
   return async e => {
     e.preventDefault();
     console.log("projClickHandler fired for url", url);
-    body.classList.add('is-modal-visible');
+    menu.innerHTML = modalContents;
+    body.classList.add('is-menu-visible');
+    // body.addEventListener("click", hideModal, {once: true});
     // console.log(e.target);
     // console.log(url);
     const apiUrl = `${url}api/wakeup`
     console.log("sending fetch to", apiUrl, Date.now());
     const res = await fetch(apiUrl);
     console.log("got something back maybe? idk, we're doing it live", Date.now())
-    body.classList.remove('is-modal-visible');
+    body.classList.remove('is-menu-visible');
+    menu.innerHTML = menuContents;
   }
 }
 
