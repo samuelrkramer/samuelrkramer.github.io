68343d362d4aacbfb1baa184c14c2be45772c685
Sam Kramer <SamuelRKramer@gmail.com>
2022-10-03 17:30:44 -0400

    also add referrer for click events, not just the page load

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -65,7 +65,7 @@ const projClickHandler = url => {
     }, 250);
     // console.log(e.target);
     // console.log(url);
-    const apiUrl = `${url}api/wakeup`
+    const apiUrl = `${url}api/wakeup/skport-click`
     // const time1 = Date.now();
     // console.log("sending fetch to", apiUrl, time1);
     const res = await fetch(apiUrl, {mode: 'no-cors'});
