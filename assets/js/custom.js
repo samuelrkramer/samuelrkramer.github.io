0cdfeec672c5427cf82341df9d9ad666de5f5a09
Sam Kramer <SamuelRKramer@gmail.com>
2023-02-27 17:06:03 -0500

    remove some commented out console.logs for cleaner code on the main event that I want to show off

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -73,31 +73,20 @@ const modalClosed = interval => {
 const projClickHandler = url => {
   return async e => {
     e.preventDefault();
-    // console.log("projClickHandler fired for url", url);
     modalLink.href = url;
     const modalContents = document.getElementById("modal").innerHTML;
     menu.innerHTML = modalContents;
     body.classList.add('is-menu-visible');
     const interval = setInterval(() => {
-      // console.log("interval checking for closed modal")
       if (!body.classList.contains("is-menu-visible")) {
-        // console.log("modal closed!");
         modalClosed(interval);
       }
     }, 250);
-    // console.log(e.target);
-    // console.log(url);
     const apiUrl = `${url}api/wakeup/skport-click`
-    // const time1 = Date.now();
-    // console.log("sending fetch to", apiUrl, time1);
     const res = await fetch(apiUrl, {mode: 'no-cors'});
-    // const time2 = Date.now();
-    // console.log("got fetch back", time2)
-    // console.log("took", (time2-time1), "ms")
     if (body.classList.contains("is-menu-visible")) {
       let newTab = window.open(url, "_blank");
       if (!newTab || newTab.closed || typeof newTab.closed=="undefined") {
-        // console.log("popup blocker detected", Date.now());
         window.location.href = url;
       }
     }
