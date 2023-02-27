af7336c4a9484334e404b0a691fc3361e1923403
Sam Kramer <SamuelRKramer@gmail.com>
2023-02-27 17:20:59 -0500

    helpful comments on star piece of code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -72,26 +72,27 @@ const modalClosed = interval => {
 
 const projClickHandler = url => {
   return async e => {
-    e.preventDefault();
+    e.preventDefault(); // intercept click on project link
     modalLink.href = url;
     const modalContents = document.getElementById("modal").innerHTML;
-    menu.innerHTML = modalContents;
-    body.classList.add('is-menu-visible');
+    menu.innerHTML = modalContents; // replace template's modal with custom modal
+    body.classList.add('is-menu-visible'); // show modal
     const interval = setInterval(() => {
-      if (!body.classList.contains("is-menu-visible")) {
+      if (!body.classList.contains("is-menu-visible")) { // restore template modal once the custom one is closed
         modalClosed(interval);
       }
     }, 250);
-    const apiUrl = `${url}api/wakeup/skport-click`
-    const res = await fetch(apiUrl, {mode: 'no-cors'});
-    if (body.classList.contains("is-menu-visible")) {
-      let newTab = window.open(url, "_blank");
-      if (!newTab || newTab.closed || typeof newTab.closed=="undefined") {
-        window.location.href = url;
+    const apiUrl = `${url}api/wakeup/skport-click`; // build target URL for wakeup fetch, doesn't matter if real
+    // here's the magic:
+    const res = await fetch(apiUrl, {mode: 'no-cors'}); // fetch to api, and wait until target is certainly awake
+    if (body.classList.contains("is-menu-visible")) { // make sure the user hasn't closed the modal before opening target
+      let newTab = window.open(url, "_blank"); // attempt new tab
+      if (!newTab || newTab.closed || typeof newTab.closed=="undefined") { // detect popup blocking
+        window.location.href = url; // redirect current tab if new tab is blocked as popup
       }
     }
-    body.classList.remove('is-menu-visible');
-    modalClosed(interval);
+    body.classList.remove('is-menu-visible'); // close modal because the target project is already open
+    modalClosed(interval); // restore template modal
   }
 }
 
